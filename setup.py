3142a212fce5ab49557421496d8dbaa60134d14f
Prasanth-Gururaj <79743886+Prasanth-Gururaj@users.noreply.github.com>
2024-10-27 19:13:56 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+hyphen_e = '-e .'
+def get_requiremenrs(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if hyphen_e in requirements : 
+            requirements.remove(hyphen_e)
+
+    return requirements
+
+setup(
+name = 'ML_Project',
+version = '0.0.1',
+authour = 'Prasanth',
+authour_email = 'prasanth4254@gmail.com',
+packages = find_packages(),
+install_requires = get_requiremenrs('requirements.txt')
+
+)
